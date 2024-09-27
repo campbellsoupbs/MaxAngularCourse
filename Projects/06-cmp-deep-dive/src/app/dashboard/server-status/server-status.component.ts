38dd77ccdd174225ffdabe0122f335eb72003e41
Brian Campbell <campbellsoupbs@gmail.com>
2024-09-27 12:41:37 -0400

    Ch. 126

--- Projects/06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ Projects/06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,7 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit{
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online'; // explicitly define allowed values for string (literal type)
+  // private interval?: ReturnType<typeof setInterval>; //set type equal to return type of setInterval()
+  private destroyRef = inject(DestroyRef)
 
+  ngOnInit() {
+    const intereval = setInterval(() => {
+      const rnd = Math.random(); // 0 - 0.999
+
+      if(rnd < 0.5) {
+        this.currentStatus = 'online'
+      } else if ( rnd < 0.9) {
+        this.currentStatus = 'online'
+      } else {
+        this.currentStatus = 'unknown'
+      }
+    }, 5000)
+
+    // Modern approach: 
+    
+    // Set up DestroyRef listener which excecutes some code when
+    // component is destroyed 
+    this.destroyRef.onDestroy(() => {
+      clearInterval(intereval);
+    });
+  }
+
+  // Old approach:
+
+  // ngOnDestroy(): void {
+  //   // Explicitly clearTimeout using id returned from setInterval (NodeJS.Timeout)
+  //   // to prevent memory leak
+  //   clearTimeout(this.interval); 
+  // }
 }
