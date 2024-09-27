67c994317376c78b9faad607e78200c83507c620
Brian Campbell <campbellsoupbs@gmail.com>
2024-09-27 15:05:37 -0400

    Ch. 134

--- Projects/06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ Projects/06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  form = viewChild.required<ElementRef<HTMLFormElement>>('form'); // Returns signal
+  
+  onSubmit(title: string, text: string) {
+    this.form().nativeElement.reset();
 
+  }
 }
--- Projects/06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ Projects/06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, contentChild, ElementRef, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -13,4 +13,6 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
 })
 export class ControlComponent {
   label = input.required<string>();
+  // ViewChild alt for projected content (w/Signals) 
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input'); 
 }
